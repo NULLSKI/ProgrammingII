228e2e7ae2481bcf7a8180af35c5d8fe181582c7
Warden0430 <60409558+Warden0430@users.noreply.github.com>
2020-05-18 13:23:58 -0500

    Create Avg3.py
    no edge cases were considered because apparently these assignments have a really low bar for functionality

---
+++ Avg3.py
@@ -0,0 +1,7 @@
+#avg3.py
+score1 = int(input('Input score one: '))
+score2 = int(input('Input score two: '))
+score3 = int(input('Input score three: '))
+n = score3 + score2 + score1
+average_score= n/3
+print('Your average score is: {}'.format(average_score))
